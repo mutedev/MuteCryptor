38225e7193242180c467395b836dbea39472175b
Myu <37182516+mutedev@users.noreply.github.com>
2021-02-24 08:26:32 +0100

    Add files via upload

---
+++ MuteCryptor/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace MuteCryptor
+{
+    partial class MuteCryptor
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tbKey = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.keyLabel = new System.Windows.Forms.Label();
+            this.btnImport = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.lblName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(12, 59);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncrypt.TabIndex = 0;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(93, 59);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDecrypt.TabIndex = 1;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 88);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(510, 292);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // tbKey
+            // 
+            this.tbKey.Location = new System.Drawing.Point(13, 30);
+            this.tbKey.Name = "tbKey";
+            this.tbKey.Size = new System.Drawing.Size(510, 23);
+            this.tbKey.TabIndex = 3;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // keyLabel
+            // 
+            this.keyLabel.AutoSize = true;
+            this.keyLabel.Location = new System.Drawing.Point(13, 12);
+            this.keyLabel.Name = "keyLabel";
+            this.keyLabel.Size = new System.Drawing.Size(26, 15);
+            this.keyLabel.TabIndex = 4;
+            this.keyLabel.Text = "Key";
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(390, 59);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(133, 23);
+            this.btnImport.TabIndex = 5;
+            this.btnImport.Text = "Import From File";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(251, 59);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(133, 23);
+            this.btnExport.TabIndex = 6;
+            this.btnExport.Text = "Export To File";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(414, 383);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(109, 15);
+            this.lblName.TabIndex = 7;
+            this.lblName.Text = "Made by MuteDevs";
+            // 
+            // MuteCryptor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(533, 407);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.btnImport);
+            this.Controls.Add(this.keyLabel);
+            this.Controls.Add(this.tbKey);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Name = "MuteCryptor";
+            this.Text = "MuteCryptor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox tbKey;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label keyLabel;
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label lblName;
+    }
+}
+
---
+++ MuteCryptor/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MuteCryptor
+{
+    public partial class MuteCryptor : Form
+    {
+        public MuteCryptor()
+        {
+            InitializeComponent();
+            richTextBox1.AllowDrop = true;
+            richTextBox1.DragDrop += richTextBox1_fileDropped;
+        }
+
+        private void richTextBox1_fileDropped(object sender, DragEventArgs e)
+        {
+            var data = e.Data.GetData(DataFormats.FileDrop);
+            if (data != null)
+            {
+                var fileNames = data as String[];
+                if (fileNames.Length > 0)
+                    richTextBox1.Text = File.ReadAllText(fileNames[0]);
+            }
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                richTextBox1.Text = Encrypt(richTextBox1.Text, tbKey.Text);
+            } 
+            catch
+            {
+
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                richTextBox1.Text = Decrypt(richTextBox1.Text, tbKey.Text);
+            }
+            catch
+            {
+
+            }
+        }
+
+        private const string initVector = "mu12temy420u69u2";
+
+        private const int keysize = 256;
+
+        public static string Encrypt(string Text, string Key)
+        {
+            byte[] initVectorBytes = Encoding.UTF8.GetBytes(initVector);
+            byte[] plainTextBytes = Encoding.UTF8.GetBytes(Text);
+            PasswordDeriveBytes password = new PasswordDeriveBytes(Key, null);
+            byte[] keyBytes = password.GetBytes(keysize / 8);
+            RijndaelManaged symmetricKey = new RijndaelManaged();
+            symmetricKey.Mode = CipherMode.CBC;
+            ICryptoTransform encryptor = symmetricKey.CreateEncryptor(keyBytes, initVectorBytes);
+            MemoryStream memoryStream = new MemoryStream();
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
+            cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+            cryptoStream.FlushFinalBlock();
+            byte[] Encrypted = memoryStream.ToArray();
+            memoryStream.Close();
+            cryptoStream.Close();
+            return Convert.ToBase64String(Encrypted);
+        }
+
+        public static string Decrypt(string EncryptedText, string Key)
+        {
+            byte[] initVectorBytes = Encoding.ASCII.GetBytes(initVector);
+            byte[] DeEncryptedText = Convert.FromBase64String(EncryptedText);
+            PasswordDeriveBytes password = new PasswordDeriveBytes(Key, null);
+            byte[] keyBytes = password.GetBytes(keysize / 8);
+            RijndaelManaged symmetricKey = new RijndaelManaged();
+            symmetricKey.Mode = CipherMode.CBC;
+            ICryptoTransform decryptor = symmetricKey.CreateDecryptor(keyBytes, initVectorBytes);
+            MemoryStream memoryStream = new MemoryStream(DeEncryptedText);
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+            byte[] plainTextBytes = new byte[DeEncryptedText.Length];
+            int decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+            memoryStream.Close();
+            cryptoStream.Close();
+            return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount);
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Browse for a file";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                richTextBox1.Text = File.ReadAllText(openFileDialog1.FileName);
+            }
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Title = "Save contents";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var file = saveFileDialog1.FileName;
+                File.WriteAllText(file, richTextBox1.Text);
+            }
+        }
+    }
+}
